6485f7cb4b885bedf3511ad42856efb207807e6c
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2024-03-01 16:23:04 +0530

    Add files via upload

---
+++ shell/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import One from "one/One";
+import Two from "two/Two";
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: shell</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+    <One />
+    <Two />
+  </div>
+);
+export default App;
---
+++ two/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+
+const App = () => (
+  <div className="container">
+    <div>Name: two</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+
+export default App;
